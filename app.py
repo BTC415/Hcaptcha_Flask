c37d58418876733aa419c1110af28dd30c7f5df9
Topcoder415 <alenschmitt415@gmail.com>
2023-10-26 02:57:55 +0900

    dynamic cpf& and proxy

--- app.py
+++ app.py
@@ -2,12 +2,7 @@ import subprocess
 import uuid
 import os
 
-from flask import (
-    Flask,
-    jsonify,
-    send_from_directory,
-    render_template
-)
+from flask import Flask, jsonify, send_from_directory, render_template, request
 
 app = Flask(__name__)
 app.debug = True
@@ -15,23 +10,31 @@ app.debug = True
 
 @app.route("/create-session", methods=["POST", "GET"])
 def create_session():
+    cpf = request.args.get("cpf","06780432627")
+    data_nascimento = request.args.get("data_nascimento","20/05/1983")
     token = str(uuid.uuid4())
 
     process = subprocess.Popen(
-        ["python3", "hcaptcha.py", token],
+        ["python3", "hcaptcha.py", token, cpf, data_nascimento],
         stdout=subprocess.PIPE,
     )
-    return render_template('page.html',token = token)
+    return render_template("page.html", token=token)
     return jsonify({"token": token})
+
 
 @app.route("/download/<upload_id>")
 def download(upload_id):
     file_path = f"{upload_id}.txt"
-    
-    if os.path.exists(file_path):
-        return send_from_directory(".", file_path, as_attachment=True)
-    else:
-        return jsonify({'message': 'Please try again later.'})
+    content = ''
+    with open(file_path, "rb") as file:
+        content = file.read().decode("utf-8")
+
+    return render_template("page.html", content=content)
+    # if os.path.exists(file_path):
+    #     return send_from_directory(".", file_path, as_attachment=True)
+    # else:
+    #     return jsonify({"message": "Please try again later."})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
--- hcaptcha.py
+++ hcaptcha.py
@@ -1,28 +1,46 @@
 import asyncio
 from pyppeteer import launch
 from capsolver_api import HCaptchaTask
+from proxy import proxy
+import random
 
 import sys
 
 token = sys.argv[1]
+cpf = sys.argv[2]
+data_nascimento = sys.argv[3]
+
+
 async def save_text_as_txt(file_path, content):
     # os.makedirs(os.path.dirname("save_datas"), exist_ok=True)
-    with open(file_path, 'w', encoding='utf-8') as file:
+    with open(file_path, "w", encoding="utf-8") as file:
         file.write(content)
 
+
 async def main():
-    url = 'https://servicos.receita.fazenda.gov.br/Servicos/CPF/ConsultaSituacao/ConsultaPublica.asp'
-    browser = await launch(options={'args': ['--no-sandbox', '--disable-setuid-sandbox']})
+    url = "https://servicos.receita.fazenda.gov.br/Servicos/CPF/ConsultaSituacao/ConsultaPublica.asp"
+    browser = await launch(
+        options={
+            "args": [
+                "--no-sandbox",
+                "--disable-setuid-sandbox",
+                f"--proxy-server=http://{proxy[random.randint(0, len(proxy))]}",
+            ]
+        }
+    )
     page = await browser.newPage()
     await page.goto(url)
-    element = await page.querySelector('#hcaptcha')
-    website_key = await page.evaluate('(element) => element.getAttribute("data-sitekey")', element)
-    capsolver = HCaptchaTask('CAP-6BF20141323EA1AEB3AF105272AE089A')
-    task_id = capsolver.create_task(task_type='HCaptchaTaskProxyLess',
-                                    website_url=url,
-                                    website_key=website_key,
-                                    is_invisible=True
-                                    )
+    element = await page.querySelector("#hcaptcha")
+    website_key = await page.evaluate(
+        '(element) => element.getAttribute("data-sitekey")', element
+    )
+    capsolver = HCaptchaTask("CAP-6BF20141323EA1AEB3AF105272AE089A")
+    task_id = capsolver.create_task(
+        task_type="HCaptchaTaskProxyLess",
+        website_url=url,
+        website_key=website_key,
+        is_invisible=True,
+    )
     while True:
         solution = capsolver.get_solution(task_id)
         if solution:
@@ -31,34 +49,43 @@ async def main():
         await asyncio.sleep(2)
     txt_cpf_element = await page.querySelector('input[name="txtCPF"]')
     if txt_cpf_element:
-        await txt_cpf_element.type("06780432627")
+        await txt_cpf_element.type(cpf)
     else:
         print("txtCPF element not found!")
-    txt_data_nascimento_element = await page.querySelector('input[name="txtDataNascimento"]')
+    txt_data_nascimento_element = await page.querySelector(
+        'input[name="txtDataNascimento"]'
+    )
     if txt_data_nascimento_element:
-        await txt_data_nascimento_element.type("20/05/1983")
+        await txt_data_nascimento_element.type(data_nascimento)
     else:
         print("txtDataNascimento element not found!")
-    await page.waitForSelector('iframe')
-    await page.evaluate('(element, captchaKey) => element.value = captchaKey',
-                        await page.querySelector('textarea[name="h-captcha-response"]'),
-                        captcha_key)
+    await page.waitForSelector("iframe")
+    await page.evaluate(
+        "(element, captchaKey) => element.value = captchaKey",
+        await page.querySelector('textarea[name="h-captcha-response"]'),
+        captcha_key,
+    )
     await page.waitFor(2000)
     btn = await page.querySelector('input[name="Enviar"]')
     await btn.click()
     await page.waitFor(5000)
     # titleSelector = "h1[class='documentFirstHeading']"
     titleSelector = "h1"
-    title = await page.evaluate('(selector) => document.querySelector(selector).textContent', titleSelector)
+    title = await page.evaluate(
+        "(selector) => document.querySelector(selector).textContent", titleSelector
+    )
     # print(await page.evaluate('(btn) => btn.getAttribute("class")', await page.querySelector('h1')))
     print(title)
     selector = "h4"
-    text = await page.evaluate('(selector) => document.querySelector(selector).textContent', selector)
+    text = await page.evaluate(
+        "(selector) => document.querySelector(selector).textContent", selector
+    )
     file_path = token + ".txt"
     savedTxt = title + "\n" + text
     await save_text_as_txt(file_path, savedTxt)
     print(text)
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(main())
     # asyncio.get_event_loop().run_until_complete(save_text_as_txt(f'{token}.txt',"HERE"))
